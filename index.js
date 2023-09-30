1ebc37d6f07b08beda717109154afd54482f9499
Lohan <ltwm1998@gmail.com>
2023-09-30 15:46:35 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+// console.log(`'multiply' is: ${multiply} \n\n`)
+
+const random = Math.floor(Math.random() * 10) + 1;
+// console.log(`'random' is: ${random} \n\n`)
+
+const num3 = 22;
+const num4 = 6;
+const mod = num3 % num4; 
+// console.log(`'mod' is: ${mod} \n\n`)
+
+const max = Math.max(10, 13, 15, 18, 20);
+// console.log(`'max' is: ${max} \n\n`)
